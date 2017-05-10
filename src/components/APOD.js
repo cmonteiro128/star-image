aec45223be9ce5e1073eb05c0ab19269b75caa79
Chris Monteiro <chris@viperapps.net>
2017-05-09 22:26:26 -0400

    Gallery sized correctly and loads 100 images efficiently

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -2,20 +2,21 @@ import React, {Component} from 'react';
 import CardView from './Card/CardView';
 import key from '../apikey.js';
 
-let cardStyle = {
-    'marginBottom' : 15,
-}
+const divStyle = {
+    padding: '8px',
+};
 
 
 class APOD extends Component {
-    
     constructor(props) {
         super(props);
         this.state = {
             imgURL: '',
             explanation: '',
             title: '',
-            subtitle: ''
+            subtitle: '',
+            listOfImages: [
+            ]
         };
         // This binding is necessary to make `this` work in the callback
         this.getImageDay = this.getImageDay.bind(this);
@@ -23,50 +24,52 @@ class APOD extends Component {
     }
 
     componentDidMount() {
-        this.getImageDay();
-        //this.getTodayApod();
+        this.getAPODS();
     }
 
-    getImageDay() {
+    getImageDay(date) {
         //Export your api key from a separate file
-        fetch('https://api.nasa.gov/planetary/apod?api_key=' + key)
+        fetch('https://api.nasa.gov/planetary/apod?api_key=' + key + '&date=' + date)
             .then((response) => {
                 console.log("Called API");
                 return response.json();
         }).then((imgData) => {
-                this.setState({
-                    imgURL: imgData['url'],
-                    explanation: imgData['explanation'],
-                    title: imgData['title'],
-                    subtitle: imgData['copyright'],    
-                })
+                let currentList = this.state.listOfImages.slice();
+                currentList.push(
+                    {
+                        imgURL: imgData['url'],
+                        title: imgData['title'],
+                        explanation: imgData['explanation'],
+                        subtitle: imgData['copyright'],
+                        date: imgData['date'],                        
+                    }    
+                )
+                this.setState({listOfImages: currentList});
         }).catch(function(err) {
             console.log(err);
         });
     }
 
-    getTodayApod() {
+    getAPODS() {
         const moment = require('moment');
-        let imageLink = 'https://apod.nasa.gov/apod/ap' + moment().format('YYMMDD') + '.html';
-        console.log(imageLink);
+        let currentDay = moment().format('YYYY-MM-DD');
+        for(let i = 0; i <= 100; i++) {
+            this.getImageDay(currentDay);
+            currentDay = moment(currentDay).add(-1, 'days').format('YYYY-MM-DD');
+        }
     }
 
-    render() {          
-        //const localImgUrl = this.imgurl; 
-        /*var rows = [];
-        for (var i=0; i < 4; i++) {
-            rows.push(<CardView key={i} style={cardStyle}/>);
-        }*/
+    render() {
         return (
             <div>
-                {/*rows*/}
                 <div className="row">
-                    <div className="col-lg-6">
-                        <CardView url={this.state.imgURL} style={cardStyle} title={this.state.title} subtitle={this.state.subtitle} explain={this.state.explanation} />
-                    </div>
-                    <div className="col-lg-6">
-                        <CardView url={this.state.imgURL} style={cardStyle} title={this.state.title} subtitle={this.state.subtitle} explain={this.state.explanation} />
-                    </div>
+                {this.state.listOfImages.map((object) => (
+                    <div key={object.date} style={divStyle} className="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                        <div className="box">
+                            <CardView url={object.imgURL} title={object.title} subtitle={this.state.subtitle} explain={this.state.explanation} />
+                        </div>
+                    </div>                
+                ))}
                 </div>                
             </div>
         );
--- src/components/Card/CardView.js
+++ src/components/Card/CardView.js
@@ -1,45 +1,32 @@
 import React from 'react';
-//import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import {GridTile} from 'material-ui/GridList';
-//import RaisedButton from 'material-ui/RaisedButton';
 import IconButton from 'material-ui/IconButton';
-//import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
-/*const styleButton = {
-  margin: 12,
-};
-
-
-/*const CardView = ({style, url, title, subtitle, explain}) => (
-  <Card style={style}>
-    <CardMedia>
-        <img height="50%" width="50%" alt='' src={url} />
-    </CardMedia>
-    <CardTitle title={title} subtitle={subtitle} />
-    <CardText>{explain}</CardText>
-    <CardActions>
-      <RaisedButton style={styleButton} primary={true} label="Share" />
-    </CardActions>
-  </Card>
-);*/
 const styleImg = {
-      position: 'relative',
-      float: 'left',
-      width: '100%',
-      minHeight: '200px',
-      minWidth: '332px',
-      overflow: 'hidden',
-      //height: '100% !important'
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  //margin: 'auto',
 };
 
-const CardView = ({style, url, title, subtitle, explain}) => (
-  <GridTile
-  title={title}
-  actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-  style={style}
-  >
-    <img alt='' style={styleImg} src={url} />
-  </GridTile>
+const styleTile = {
+      overflow: 'hidden',
+      position: 'relative',
+      'paddingBottom': '60%'
+};
+
+const CardView = ({url, title, subtitle, explain}) => (
+
+    <GridTile
+    title={title}
+    actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+    containerElement={'div'}
+    style={styleTile}
+    >
+      <img alt='' style={styleImg} src={url} />
+    </GridTile>
 );
 export default CardView;
