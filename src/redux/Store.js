c91767fbedd328dbb9fd728c246e4ed89551771d
Chris Monteiro <chris@viperapps.net>
2017-06-08 16:51:44 -0400

    Unejected, updated create-react script. Now PWA, uses Webpack 2, and initial start of storing image info in Redux store

---
+++ src/redux/Store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers } from 'redux';
+
+// The User Reducer
+const userReducer = function(state = {}, action) {
+  return state;
+}
+
+// The Widget Reducer
+const widgetReducer = function(state = {}, action) {
+  return state;
+}
+
+// Combine Reducers
+const reducers = combineReducers({
+  userState: userReducer,
+  widgetState: widgetReducer
+});
+
+const store = createStore(reducers);
